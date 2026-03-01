8b1c0e57411c2d4d5d65cebc17b1977701c3ca04
JaviPC <jbelloncordobes@gmail.com>
2026-03-01 22:30:47 +0100

    Fixed memory leak in switching scenes and UI positioning issues

---
+++ Assets/Scripts/GameManagerSetup.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int playerHealth = 100;
+    public int playerHunger = 100;
+    public int score = 0;
+
+    private void Awake()
+    {
+        // If no instance exists, this becomes the singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/NightTimerUI.cs
+++ Assets/Scripts/NightTimerUI.cs
@@ -1,35 +1,34 @@
 using UnityEngine;
 using TMPro;
 
-public class DayTimerUI : MonoBehaviour
+public class NightTimerUI : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     // public TextMeshProUGUI dayNightText;
 
-    private DayTimer dayTimer;
+    private NightTimer nightTimer;
 
     void Start()
     {
-        dayTimer = FindFirstObjectByType<DayTimer>();
-        Debug.Log("DayTimerUI Start - DayTimer found: " + (dayTimer != null));
+        nightTimer = FindFirstObjectByType<NightTimer>();
+        Debug.Log("NightTimerUI Start - NightTimer found: " + (nightTimer != null));
         Debug.Log("TimerText assigned: " + (timerText != null));
-        // Debug.Log("DayNightText assigned: " + (dayNightText != null));
     }
 
     void Update()
     {
-        if (dayTimer == null)
+        if (nightTimer == null)
         {
-            dayTimer = FindFirstObjectByType<DayTimer>();
-            Debug.Log("Searching for DayTimer...");
+            nightTimer = FindFirstObjectByType<NightTimer>();
+            Debug.Log("Searching for NightTimer...");
             return;
         }
 
-        timerText.text = dayTimer.GetTimeFormatted();
+        timerText.text = nightTimer.GetTimeFormatted();
         // dayNightText.text = dayTimer.isDay ? "Day" : "Night";
         // dayNightText.text = "Day";
 
-        if (dayTimer.timeRemaining <= 60f)
+        if (nightTimer.timeRemaining <= 60f)
             timerText.color = Color.Lerp(Color.red, Color.white,
                 Mathf.PingPong(Time.time * 2f, 1f));
         else

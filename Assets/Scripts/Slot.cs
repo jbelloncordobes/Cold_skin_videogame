c6b4b4617d2b2be47842591e45e70d36aa36375b
jbelloncordobes <jbelloncordobes@gmail.com>
2026-03-04 00:26:26 +0100

    Simple inventory management

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject currentItem; // The item currently held in this slot
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+}
